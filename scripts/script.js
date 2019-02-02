f41e0cc8621ef5d84de5bed575675dd896573892
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-02 16:40:52 +0000

    Traversing the DOM with Jquery, all diff element selectors

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,25 @@
-// ----------------- jQuery Filters -------------
+// ----------------- transversing the DOM with jQuery -------------
 
-    // selecting first and last child
-$("header nav li:first").css({border: "2px solid red"});
-$("header nav li:last").css({border: "2px solid red"});
+// selects the next element after the one mentioned
+// $("#contact-methods").next().css({border: "3px solid red"})
 
-    // selecting first and last ul DOM elements
-$("#contact ul:first").css({border: "2px solid red"});
-$("#contact ul:last").css({border: "2px solid red"});
+// selects the previous element after the one mentioned
+// $("#social-nav").prev().css({border: "3px solid red"});
 
-    // selecting first and last ul child DOM elements
-$("#contact ul:first-child").css({border: "2px solid red"});
-$("#contact ul:last-child").css({border: "2px solid red"});
+// selects the first parent of an element
+// $(".banner-title").parent().css({border: "3px solid pink"});
 
-    // selecting even or odd elements
-$("header nav li:even").css({border: "2px solid red"});
-$("header nav li:odd").css({border: "2px solid yellow"});
+// selects all the parents (grandparents, great grandparents, etc)
+// Just add an s 
+// $("#social-nav").parents().css({border: "3px solid red"});
 
-    // select me all the sections that do not match #contact need to use single quotes
-$("section:not('#contact')").css({border: "2px solid green"});
+// select all the childen of an element 
+// $("#social-nav").children().css({border: "3px solid green"});
 
-    // select all the elements less than number in parentheses
-$("#social-nav li:lt(3)").css({border: "2px solid blue"});
-    
-    // select all the elements greater than number in parentheses
-$("#social-nav li:gt(2)").css({border: "2px solid blue"});
+// find an element within a section and select that
+// $("#contact").find(".facebook").css({border: "3px solid purple"});
 
-    // select elements by attribute
-$("img[alt]").css({border: "2px solid pink"});
-
-    // select elements by attribute with specific value
-$("img[alt=quote]").css({border: "2px solid pink"});
+// grab the closest element in relation to another element
+$("#social-nav").closest(".wrapper").css({border: "3px solid red"});
 
 
