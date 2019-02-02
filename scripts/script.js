d5f8b8e68f7a67c3dad4f92f8d9f4b88c390a190
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-02 16:57:59 +0000

    jQuery chaining

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,8 @@
-// ----------------- transversing the DOM with jQuery -------------
+// ----------------- jQuery chaining -------------
+// linking together more than one elemnt manioulation in one statement
 
-// selects the next element after the one mentioned
-// $("#contact-methods").next().css({border: "3px solid red"})
-
-// selects the previous element after the one mentioned
-// $("#social-nav").prev().css({border: "3px solid red"});
-
-// selects the first parent of an element
-// $(".banner-title").parent().css({border: "3px solid pink"});
-
-// selects all the parents (grandparents, great grandparents, etc)
-// Just add an s 
-// $("#social-nav").parents().css({border: "3px solid red"});
-
-// select all the childen of an element 
-// $("#social-nav").children().css({border: "3px solid green"});
-
-// find an element within a section and select that
-// $("#contact").find(".facebook").css({border: "3px solid purple"});
-
-// grab the closest element in relation to another element
-$("#social-nav").closest(".wrapper").css({border: "3px solid red"});
+$("#contact-methods").css({border: "3px solid red"})
+    .next().css({border: "2px solid green"})
+    .closest('section').css({border: "2px solid blue"});
 
 
