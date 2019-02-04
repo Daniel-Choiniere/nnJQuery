3799785e920f3a87a351a914afab1c683b3aaf1d
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-04 20:33:34 +0000

    merged version10.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,19 @@
 /*global $*/
-//  // ----------------- CSS With Jquery -------------
+//  // ----------------- Adding and removing classes -------------
 
-// // check value of a certain property already given to that element 
-// console.log($("#social-nav").css("position"))
-// console.log($("#social-nav").css("top"));
+// removeClass() - removes a class from the mathced element(s)
+// addCLass() - adds a class to the mathced element(s)
+// toggleClass - toggles the class on and off on there matched elements
 
-// // change the value of a property of an element
-// ($("#social-nav").css("top", "-200px"))
-// // can chain multiple properties
-// $("#social-nav").css("top", "-200px").css("left", "100px").css;
+// $("header .wrapper").removeClass("wrapper");
 
+// $("header > div").addClass("wrapper");
 
-// creating an object to define multiple CSS properties
-$("#social-nav").css({
-   "top" : "-400px",
-   "left" : "150px",
-   "opacity" : "0.5",
-   "border-top" : "4px solid red"
-});
+// $("header .wrapper").removeClass("wrapper");
+
+var button = $("#lead-banner a");
+
+button[0].onclick = function(){
+    $("#points-of-sale").toggleCLass("open");
+    return false;
+};
