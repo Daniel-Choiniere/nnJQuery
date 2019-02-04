7b6515f3339a47aaa4bf12f5a71f73289d74fc65
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-04 20:45:48 +0000

    binding and unbinding events, ex) background color change via  onclick event

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,14 @@
 /*global $*/
-//  // ----------------- Adding and removing classes -------------
+//  // ----------------- Binding and Unbinding Events -------------
 
-// removeClass() - removes a class from the mathced element(s)
-// addCLass() - adds a class to the mathced element(s)
-// toggleClass - toggles the class on and off on there matched elements
+// on() - binds an event to matched element(s)
+// off() - unbinds event from matched element(s)
 
-// $("header .wrapper").removeClass("wrapper");
 
-// $("header > div").addClass("wrapper");
+var myLis = $("#points-of-sale li");
 
-// $("header .wrapper").removeClass("wrapper");
-
-var button = $("#lead-banner a");
-
-button[0].onclick = function(){
-    $("#points-of-sale").toggleCLass("open");
-    return false;
-};
+myLis.on("click", function(){
+   $(this).css({"background" : "pink"});
+   
+   myLis.off("click")
+});
