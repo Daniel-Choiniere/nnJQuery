a0f2096c22cbfad06b384fc50309cbe617b5bf65
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-04 21:23:01 +0000

    document ready vs window load

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,12 @@
 /*global $*/
-//  // ----------------- Event Helpers -------------
+//  // ----------------- Doscumentt Ready vs Windows Load -------------
+//   if i want to or need to put my script tags (links) in the head as opposed to at the bottom of my body i need to include either docimnet ready or window load function
 
-// on() - binds an event to matched element(s)
-// off() - unbinds event from matched element(s)
-
-
-
-// $("#lead-banner").dblclick(function(){
-//   alert("you double clicked me brahhh");
-// });
-
-// works exactly the same as
-
-$("#lead-banner").on("dblclick", function(){
-   alert("you double clicked me brahhh");
+$(function() {
+    
+    // inside this function would be all the 
+    
+    
 });
 
-// PLAY AROUND WITH MORE EVENT HELPERS ON YOUR OWN TIME
+// waits for the whole document to load then fire your script code
