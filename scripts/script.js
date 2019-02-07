4108ccc75f64e88b15ab8ca39f4fdf7403de6f70
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-07 16:16:49 +0000

    fading animation example, cycles through list of quotes and fades each one in and out one at a time

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,23 @@
 /*global $*/
-//  // ----------------- Sliding Elements -------------
+//  // ----------------- Fading Animation Example -------------
 
-
-// $("document").ready(function(){
-
-//     $(".slide-button-up").on("click", function(){
-//         $("#lead-banner").slideUp(2000);
-//     });
-    
-//      $(".slide-button-down").on("click", function(){
-//         $("#lead-banner").slideDown(500, function() {
-//             alert("animation complete!");
-//         });
-//     });
-// });
-
-        // Makes the one button a toggle that allows the same button to go up and down  
-
+// makes the quote section cycle through all the quotes and restart at the beginning once it has finished (each quote fading in and out on a delay)
 $("document").ready(function(){
-
-    $(".slide-button-up").on("click", function(){
-        $("#lead-banner").slideToggle(2000);
-    });
+    
+    let allQuotes = $("blockquote");
+    let currentQuote = 0;
+    
+    function changeQuote(){
+        $(allQuotes[currentQuote]).fadeOut(200);
+        
+        if(currentQuote == allQuotes.length -1) {
+            currentQuote = 0;
+        } else {
+            currentQuote++;
+        }
+        $(allQuotes[currentQuote]).fadeIn(200);
+        
+    }
+    let quoteTimer = setInterval(changeQuote, 1500);
+    
 });
