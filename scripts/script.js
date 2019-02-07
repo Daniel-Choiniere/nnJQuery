ab0da1aa7bf4adf178b42bbceb05f8097375f503
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-07 15:54:55 +0000

    sliding elements, creates a cool functional aniamtion that acts like a bootstrap accordion, pulling an elemnt in and out of view

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,25 @@
 /*global $*/
-//  // ----------------- Hiode and Show Elements -------------
+//  // ----------------- Sliding Elements -------------
 
+
+// $("document").ready(function(){
+
+//     $(".slide-button-up").on("click", function(){
+//         $("#lead-banner").slideUp(2000);
+//     });
+    
+//      $(".slide-button-down").on("click", function(){
+//         $("#lead-banner").slideDown(500, function() {
+//             alert("animation complete!");
+//         });
+//     });
+// });
+
+        // Makes the one button a toggle that allows the same button to go up and down  
 
 $("document").ready(function(){
 
-    $("img[alt=map]").on("click", function() {
-    // $("section > h2").on("click", function() {
-        
-        // element shrinks and animates to the left
-        // $(this).hide(1000);
-        
-        // animates (shrinks and mobves to the left, then animates back ame way when show is called)
-        // $(this).hide(1000).show(1000);1
-        
-        $("section > h2").toggle(1000);
-        
-        });
+    $(".slide-button-up").on("click", function(){
+        $("#lead-banner").slideToggle(2000);
+    });
 });
