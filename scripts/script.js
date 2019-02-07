b5ee873b8bc84ce5c487a269c6eb1205205977ed
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-07 15:24:23 +0000

    fading elements inand out

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,27 @@
 /*global $*/
-//  // ----------------- Jquery animations -------------
-// I want to show you how to access the event object during events (such as click events) in your code.
+//  // ----------------- Fading elements in and out -------------
+
 
 $("document").ready(function(){
-    
-    function complete(){
-     alert("Animation complete!")   
-    }
+
     
     $("section > h2").on("click", function() {
         // can only animate items values that are numeric
             // third parameter is the speed of animation
             // fourth parameter is if it speeds up linear or other
             // fifth parameter an optional function to fire at end of animation (ex. after user buys an item a small img of the bought item animated to the "checkout cart" and then a window pops up saying that the item has been added to the cart)
-        $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", complete);
-    });
+        // $(this).fadeOut(3000).fadeIn(1500);
+       
+    //   makes a cool flashing effect fadeing in and out ovr and over
+        $(this).fadeTo(200, 0.2)
+                .fadeTo(200, 0.8)
+                .fadeTo(200, 0.2)
+                .fadeTo(200, 0.8)
+                .fadeTo(200, 0.2)
+                .fadeTo(200, 0.8)
+                .fadeTo(200, 0.2)
+                .fadeTo(200, 0.8)
+                .fadeTo(200, 0.2)
+                .fadeTo(200, 0.2);
+        });
 });
