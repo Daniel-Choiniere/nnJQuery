2bad44e7ebe7f60de2e5d005caa45ac7903affdc
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-07 17:09:18 +0000

    using Jquery plugins to make animations, etc. easier. made the quotes fade in and out with one line of code

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,10 @@
 /*global $*/
-//  // ----------------- Click to Expand Sliding Animation Example -------------
+//  // ----------------- Jquery Plugins -------------
 
 // when elements are clicked the paragraph below slides down and toggles back when clicked again
 
 $("document").ready(function(){
 
-    let items = $("#points-of-sale li");
-    
-    items.on("click", function(){
-       $(this).find("p").slideToggle(500); 
-    });
+    $(".rslides").responsiveSlides();
 
 });
